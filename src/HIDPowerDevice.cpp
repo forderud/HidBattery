6b5b2d42f962c70fe6e56faa8e23ef50e36bec05
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-01-24 09:32:15 +0100

    Add CycleCount parameter
    Done to make the emulated battery more closely match an actual battery. Initialize CycleCount to 41 to make it interesting.
    
    WARNING: The parameter does not seem to be picked up by the Windows HidBatt driver (tested on Win10 22H2 and Win11 24H).

--- src/HIDPowerDevice.cpp
+++ src/HIDPowerDevice.cpp
@@ -104,6 +104,15 @@ static const uint8_t s_hidReportDescriptor[] PROGMEM = {
     0x09, 0x68, //     USAGE (RunTimeToEmpty)
     0xB1, 0xA3, //     FEATURE (Constant, Variable, Absolute, No Wrap, Linear, No Preferred, No Null Position, Volatile, Bitfield)
 
+    0x85, HID_PD_CYCLE_COUNT, //     REPORT_ID (20)
+    0x09, 0x6B, //     USAGE (CycleCount)
+    0x75, 0x10, //     REPORT_SIZE (16)
+    0x15, 0x00, //     LOGICAL_MINIMUM (0)
+    0x27, 0xFF, 0xFF, 0x00, 0x00, //     LOGICAL_MAXIMUM (65534)
+    0x81, 0x22, //     INPUT (Data, Variable, Absolute, No Wrap, Linear, No Preferred, No Null Position, Bitfield)
+    0x09, 0x6B, //     USAGE (CycleCount)
+    0xB1, 0xA2, //     FEATURE (Data, Variable, Absolute, No Wrap, Linear, No Preferred, No Null Position, Volatile, Bitfield)
+
     0x05, 0x84, //     USAGE_PAGE (Power Device) ====================
     0x85, HID_PD_VOLTAGE, //     REPORT_ID (11)
     0x09, 0x30, //     USAGE (Voltage)
