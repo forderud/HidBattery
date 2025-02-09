d67e094a98623889bafe72013245b5493b4dec04
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-10 00:09:10 +0100

    Add bmRequestType checking for string descriptors.

--- src/HID/HID.cpp
+++ src/HID/HID.cpp
@@ -54,14 +54,16 @@ int HID_::getDescriptor(USBSetup& setup)
     if (setup.bRequest != GET_DESCRIPTOR) // redundant check, since it's already done before calling this method
         return 0;
     
-    if (setup.wIndex == 0x0409) {// English (matches STRING_LANGUAGE in arduino/USBCore.cpp)
-        // HID-specific strings
-        if(setup.wValueH == USB_STRING_DESCRIPTOR_TYPE) {
-            HIDReport* rep = GetFeature(setup.wValueL, true/*string*/);
-            if(rep)
-                return USB_SendStringDescriptor((char*)rep->data, strlen_P((char*)rep->data), TRANSFER_PGM);
-            else
-                return 0;
+    if (setup.bmRequestType == REQUEST_DEVICETOHOST | REQUEST_STANDARD | REQUEST_DEVICE) {
+        if (setup.wIndex == 0x0409) {// English (matches STRING_LANGUAGE in arduino/USBCore.cpp)
+            // HID-specific strings
+            if(setup.wValueH == USB_STRING_DESCRIPTOR_TYPE) {
+                HIDReport* rep = GetFeature(setup.wValueL, true/*string*/);
+                if(rep)
+                    return USB_SendStringDescriptor((char*)rep->data, strlen_P((char*)rep->data), TRANSFER_PGM);
+                else
+                    return 0;
+            }
         }
     }
 
