d4064fc510e74a99ab8a7c63021e3931b78e7223
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-10 12:30:18 +0100

    Remove i/b type prefixes from local variables.

--- battery/battery.ino
+++ battery/battery.ino
@@ -9,25 +9,25 @@ const char STRING_OEMVENDOR[] PROGMEM = "BatteryVendor";
 
 const char STRING_SERIAL[] PROGMEM = "1234";
 
-PresentStatus iPresentStatus = {};
+PresentStatus PresentStatus = {};
 
-byte bCapacityMode = 0;  // unit: 0=mAh, 1=mWh, 2=%
+byte CapacityMode = 0;  // unit: 0=mAh, 1=mWh, 2=%
 
 // Physical parameters
-uint16_t iVoltage =1499; // centiVolt
-uint16_t iRunTimeToEmpty = 0; // maps to BatteryEstimatedTime on Windows
-uint16_t iManufacturerDate = 0; // initialized in setup function
-int16_t  iCycleCount = 41;
-uint16_t iTemperature = 300; // degrees Kelvin
+uint16_t Voltage =1499; // centiVolt
+uint16_t RunTimeToEmpty = 0; // maps to BatteryEstimatedTime on Windows
+uint16_t ManufacturerDate = 0; // initialized in setup function
+int16_t  CycleCount = 41;
+uint16_t Temperature = 300; // degrees Kelvin
 
 // Parameters for ACPI compliancy
-const uint16_t iDesignCapacity = 58003*360/iVoltage; // AmpSec=mWh*360/centiVolt (1 mAh = 3.6 As)
-uint16_t iRemnCapacityLimit = iDesignCapacity/20; // critical at 5% (maps to DefaultAlert1 on Windows)
-uint16_t iWarnCapacityLimit = iDesignCapacity/10; // low  at 10% (maps to DefaultAlert2 on Windows)
-uint16_t iFullChargeCapacity = 40690*360/iVoltage; // AmpSec=mWh*360/centiVolt (1 mAh = 3.6 As)
+const uint16_t DesignCapacity = 58003*360/Voltage; // AmpSec=mWh*360/centiVolt (1 mAh = 3.6 As)
+uint16_t RemnCapacityLimit = DesignCapacity/20; // critical at 5% (maps to DefaultAlert1 on Windows)
+uint16_t WarnCapacityLimit = DesignCapacity/10; // low  at 10% (maps to DefaultAlert2 on Windows)
+uint16_t FullChargeCapacity = 40690*360/Voltage; // AmpSec=mWh*360/centiVolt (1 mAh = 3.6 As)
 
-uint16_t iRemaining[MAX_BATTERIES] = {}; // remaining charge
-uint16_t iPrevRemaining=0;
+uint16_t Remaining[MAX_BATTERIES] = {}; // remaining charge
+uint16_t PrevRemaining=0;
 
 HIDPowerDevice_ PowerDevice[MAX_BATTERIES];
 
@@ -39,7 +39,7 @@ void setup() {
 
   for (int i = 0; i < MAX_BATTERIES; i++) {
     // initialize batteries with 30% charge
-    iRemaining[i] = 0.30f*iFullChargeCapacity;
+    Remaining[i] = 0.30f*FullChargeCapacity;
 
     PowerDevice[i].SetManufacturer(STRING_OEMVENDOR);
     PowerDevice[i].SetSerial(STRING_SERIAL);
@@ -48,84 +48,84 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);  // output flushing 1 sec indicating that the arduino cycle is running.
 
   for (int i = 0; i < MAX_BATTERIES; i++) {
-    PowerDevice[i].SetFeature(HID_PD_PRESENTSTATUS, &iPresentStatus, sizeof(iPresentStatus));
+    PowerDevice[i].SetFeature(HID_PD_PRESENTSTATUS, &PresentStatus, sizeof(PresentStatus));
 
-    PowerDevice[i].SetFeature(HID_PD_RUNTIMETOEMPTY, &iRunTimeToEmpty, sizeof(iRunTimeToEmpty));
+    PowerDevice[i].SetFeature(HID_PD_RUNTIMETOEMPTY, &RunTimeToEmpty, sizeof(RunTimeToEmpty));
 
-    PowerDevice[i].SetFeature(HID_PD_CAPACITYMODE, &bCapacityMode, sizeof(bCapacityMode));
-    PowerDevice[i].SetFeature(HID_PD_TEMPERATURE, &iTemperature, sizeof(iTemperature));
-    PowerDevice[i].SetFeature(HID_PD_VOLTAGE, &iVoltage, sizeof(iVoltage));
+    PowerDevice[i].SetFeature(HID_PD_CAPACITYMODE, &CapacityMode, sizeof(CapacityMode));
+    PowerDevice[i].SetFeature(HID_PD_TEMPERATURE, &Temperature, sizeof(Temperature));
+    PowerDevice[i].SetFeature(HID_PD_VOLTAGE, &Voltage, sizeof(Voltage));
 
     PowerDevice[i].SetStringIdxFeature(HID_PD_IDEVICECHEMISTRY, &DeviceChemistryIdx, STRING_DEVICECHEMISTRY);
 
-    PowerDevice[i].SetFeature(HID_PD_DESIGNCAPACITY, &iDesignCapacity, sizeof(iDesignCapacity));
-    PowerDevice[i].SetFeature(HID_PD_FULLCHRGECAPACITY, &iFullChargeCapacity, sizeof(iFullChargeCapacity));
-    PowerDevice[i].SetFeature(HID_PD_REMAININGCAPACITY, &iRemaining[i], sizeof(iRemaining[i]));
-    PowerDevice[i].SetFeature(HID_PD_REMNCAPACITYLIMIT, &iRemnCapacityLimit, sizeof(iRemnCapacityLimit));
-    PowerDevice[i].SetFeature(HID_PD_WARNCAPACITYLIMIT, &iWarnCapacityLimit, sizeof(iWarnCapacityLimit));
+    PowerDevice[i].SetFeature(HID_PD_DESIGNCAPACITY, &DesignCapacity, sizeof(DesignCapacity));
+    PowerDevice[i].SetFeature(HID_PD_FULLCHRGECAPACITY, &FullChargeCapacity, sizeof(FullChargeCapacity));
+    PowerDevice[i].SetFeature(HID_PD_REMAININGCAPACITY, &Remaining[i], sizeof(Remaining[i]));
+    PowerDevice[i].SetFeature(HID_PD_REMNCAPACITYLIMIT, &RemnCapacityLimit, sizeof(RemnCapacityLimit));
+    PowerDevice[i].SetFeature(HID_PD_WARNCAPACITYLIMIT, &WarnCapacityLimit, sizeof(WarnCapacityLimit));
 
     uint16_t year = 2024, month = 10, day = 12;
-    iManufacturerDate = (year - 1980)*512 + month*32 + day; // from 4.2.6 Battery Settings in "Universal Serial Bus Usage Tables for HID Power Devices"
-    PowerDevice[i].SetFeature(HID_PD_MANUFACTUREDATE, &iManufacturerDate, sizeof(iManufacturerDate));
+    ManufacturerDate = (year - 1980)*512 + month*32 + day; // from 4.2.6 Battery Settings in "Universal Serial Bus Usage Tables for HID Power Devices"
+    PowerDevice[i].SetFeature(HID_PD_MANUFACTUREDATE, &ManufacturerDate, sizeof(ManufacturerDate));
 
-    PowerDevice[i].SetFeature(HID_PD_CYCLE_COUNT, &iCycleCount, sizeof(iCycleCount));
+    PowerDevice[i].SetFeature(HID_PD_CYCLE_COUNT, &CycleCount, sizeof(CycleCount));
   }
 }
 
 void loop() {
   // propagate charge level from first to last battery
   for (int i = MAX_BATTERIES-1; i > 0; i--)
-    iRemaining[i] = iRemaining[i-1];
+    Remaining[i] = Remaining[i-1];
 
 #ifdef ENABLE_POTENTIOMETER
   // read charge level from potentiometer
   int iBattSoc = analogRead(PIN_A7); // potentiometer value in [0,1024)
-  iRemaining[0] = (uint16_t)(round((float)iFullChargeCapacity*iBattSoc/1024));
+  Remaining[0] = (uint16_t)(round((float)FullChargeCapacity*iBattSoc/1024));
 
-  if (iRemaining[0] > iPrevRemaining + 1) // add a bit hysteresis
-    iPresentStatus.Charging = true;
-  else if (iRemaining[0] < iPrevRemaining - 1) // add a bit hysteresis
-    iPresentStatus.Charging = false;
+  if (Remaining[0] > PrevRemaining + 1) // add a bit hysteresis
+    PresentStatus.Charging = true;
+  else if (Remaining[0] < PrevRemaining - 1) // add a bit hysteresis
+    PresentStatus.Charging = false;
 #else
   // simulate charge & discharge cycles
-  if (iPresentStatus.Charging) {
-    iRemaining[0] += 0.02f*iFullChargeCapacity; // incr. 2%
+  if (PresentStatus.Charging) {
+    Remaining[0] += 0.02f*FullChargeCapacity; // incr. 2%
 
-    if (iRemaining[0] > iFullChargeCapacity) {
-      iRemaining[0] = iFullChargeCapacity; // clamp at 100%
-      iPresentStatus.Charging = false;
+    if (Remaining[0] > FullChargeCapacity) {
+      Remaining[0] = FullChargeCapacity; // clamp at 100%
+      PresentStatus.Charging = false;
     }
   } else {
-    iRemaining[0] -= 0.02f*iFullChargeCapacity; // decr. 2%
+    Remaining[0] -= 0.02f*FullChargeCapacity; // decr. 2%
 
-    if (iRemaining[0] < 0.20f*iFullChargeCapacity) {
-      iRemaining[0] = 0.20f*iFullChargeCapacity; // clamp at 20% to prevent triggering shutdown
-      iPresentStatus.Charging = true;
-      iCycleCount += 1;
+    if (Remaining[0] < 0.20f*FullChargeCapacity) {
+      Remaining[0] = 0.20f*FullChargeCapacity; // clamp at 20% to prevent triggering shutdown
+      PresentStatus.Charging = true;
+      CycleCount += 1;
     }
   }
 #endif
 
   uint16_t iAvgTimeToEmpty = 7200;
-  iRunTimeToEmpty = (uint16_t)round((float)iAvgTimeToEmpty*iRemaining[0]/iFullChargeCapacity);
+  RunTimeToEmpty = (uint16_t)round((float)iAvgTimeToEmpty*Remaining[0]/FullChargeCapacity);
 
   // Charging
-  iPresentStatus.ACPresent = iPresentStatus.Charging; // assume charging implies AC present
+  PresentStatus.ACPresent = PresentStatus.Charging; // assume charging implies AC present
   // Discharging
-  if(!iPresentStatus.Charging) { // assume not charging implies discharging
-    iPresentStatus.Discharging = 1;
+  if(!PresentStatus.Charging) { // assume not charging implies discharging
+    PresentStatus.Discharging = 1;
   } else {
-    iPresentStatus.Discharging = 0;
+    PresentStatus.Discharging = 0;
   }
 
   // Shutdown imminent
-  if(iRunTimeToEmpty < 60) {
-    iPresentStatus.ShutdownImminent = 1;
+  if(RunTimeToEmpty < 60) {
+    PresentStatus.ShutdownImminent = 1;
 #ifdef CDC_ENABLED
     Serial.println("shutdown imminent");
 #endif
   } else {
-    iPresentStatus.ShutdownImminent = 0;
+    PresentStatus.ShutdownImminent = 0;
   }
 
   //************ Delay ****************************************
@@ -138,26 +138,26 @@ void loop() {
   int res = 0;
   for (int i = 0; i < MAX_BATTERIES; i++) {
     if (res >= 0)
-      res = PowerDevice[i].SendReport(HID_PD_REMAININGCAPACITY, &iRemaining[i], sizeof(iRemaining[i]));
+      res = PowerDevice[i].SendReport(HID_PD_REMAININGCAPACITY, &Remaining[i], sizeof(Remaining[i]));
 
-    if((res >= 0) && !iPresentStatus.Charging)
-      res = PowerDevice[i].SendReport(HID_PD_RUNTIMETOEMPTY, &iRunTimeToEmpty, sizeof(iRunTimeToEmpty));
-
-    if (res >= 0)
-      PowerDevice[i].SetFeature(HID_PD_TEMPERATURE, &iTemperature, sizeof(iTemperature));
+    if((res >= 0) && !PresentStatus.Charging)
+      res = PowerDevice[i].SendReport(HID_PD_RUNTIMETOEMPTY, &RunTimeToEmpty, sizeof(RunTimeToEmpty));
 
     if (res >= 0)
-      res = PowerDevice[i].SendReport(HID_PD_PRESENTSTATUS, &iPresentStatus, sizeof(iPresentStatus));
+      PowerDevice[i].SetFeature(HID_PD_TEMPERATURE, &Temperature, sizeof(Temperature));
 
     if (res >= 0)
-      res = PowerDevice[i].SendReport(HID_PD_CYCLE_COUNT, &iCycleCount, sizeof(iCycleCount));
+      res = PowerDevice[i].SendReport(HID_PD_PRESENTSTATUS, &PresentStatus, sizeof(PresentStatus));
+
+    if (res >= 0)
+      res = PowerDevice[i].SendReport(HID_PD_CYCLE_COUNT, &CycleCount, sizeof(CycleCount));
   }
 
-  iPrevRemaining = iRemaining[0];
+  PrevRemaining = Remaining[0];
 
 #ifdef CDC_ENABLED
   Serial.print("Remaining charge: ");
-  Serial.println(iRemaining[0]);
+  Serial.println(Remaining[0]);
 
   Serial.print("SendReport res=");
   Serial.println(res);
