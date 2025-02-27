038eaa8fbcd554dd2541e619c842495aa808a9b4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-27 12:33:15 +0100

    Log "Information" value from lower HidBatt driver in IOCTL completion routine.

--- HidBattExt/Battery.cpp
+++ HidBattExt/Battery.cpp
@@ -71,7 +71,7 @@ void EvtIoDeviceControlBattFilterCompletion (_In_  WDFREQUEST Request, _In_  WDF
     status = WdfRequestRetrieveOutputBuffer(Request, 0, (void**)&OutputBuffer, &OutputBufferLength);
     NT_ASSERTMSG("WdfRequestRetrieveOutputBuffer failed", NT_SUCCESS(status));
 
-    DebugPrint(DPFLTR_INFO_LEVEL, "EvtIoDeviceControlBattFilterCompletion: IOCTL_BATTERY_QUERY_INFORMATION (InformationLevel=%u, OutputBufferLength=%u)\n", reqCtx->InformationLevel, OutputBufferLength);
+    DebugPrint(DPFLTR_INFO_LEVEL, "EvtIoDeviceControlBattFilterCompletion: IOCTL_BATTERY_QUERY_INFORMATION (InformationLevel=%u, OutputBufferLength=%u, Information=%u)\n", reqCtx->InformationLevel, OutputBufferLength, WdfRequestGetInformation(Request));
 
     if ((reqCtx->InformationLevel == BatteryInformation) && (OutputBufferLength == sizeof(BATTERY_INFORMATION))) {
         auto* bi = (BATTERY_INFORMATION*)OutputBuffer;
