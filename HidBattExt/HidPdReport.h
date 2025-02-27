0c4e339a56c210de76a7e6b0b4285f7fffa502ff
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-27 15:21:32 +0100

    Remove unused HidPdReport self-print methods.

--- HidBattExt/HidPdReport.h
+++ HidBattExt/HidPdReport.h
@@ -7,17 +7,6 @@ struct HidPdReport {
         ReportId = type;
     }
 
-#ifdef _KERNEL_MODE
-    void Print(const char* prefix) const {
-        DebugPrint(DPFLTR_INFO_LEVEL, "HidBattExt: %s ReportID=0x%x, Value=%u\n", prefix, ReportId, Value); prefix;
-    }
-#else
-    void Print(const char* prefix) const {
-        printf("%s Report=%s, Value=%u\n", prefix, TypeStr((ReportType)ReportId), Value);
-    }
-#endif
-
-    //report ID of the collection to which the control request is sent
     UCHAR  ReportId = 0;
     USHORT Value = 0;
 
