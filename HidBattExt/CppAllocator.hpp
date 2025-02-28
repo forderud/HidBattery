53ea0bbabdfedfd9fa0ead62dced392baf883a90
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-28 21:32:14 +0100

    Remove C++ new & delete impl. that are no longer used.

--- HidBattExt/CppAllocator.hpp
+++ HidBattExt/CppAllocator.hpp
@@ -3,24 +3,6 @@
 /* This codes assumes that POOL_TAG have already been defined if building in kernel-mode. */
 
 #ifdef _KERNEL_MODE
-void* operator new (size_t size) noexcept {
-    // allocate in non-paged pool (will always reside in RAM)
-    return ExAllocatePool2(POOL_FLAG_NON_PAGED, size, POOL_TAG);
-}
-
-_Ret_writes_maybenull_(size)
-void* operator new[] (size_t size) noexcept {
-    // allocate in non-paged pool (will always reside in RAM)
-    return ExAllocatePool2(POOL_FLAG_NON_PAGED, size, POOL_TAG);
-}
-
-void operator delete (void *ptr, size_t /*size*/) noexcept {
-    return ExFreePoolWithTag(ptr, POOL_TAG);
-}
-
-void operator delete[] (void* ptr) noexcept {
-    return ExFreePoolWithTag(ptr, POOL_TAG);
-}
 
 /** Templatized RAII array class for physical RAM allocations that won't be paged out (non-paged). */
 template<class T>
