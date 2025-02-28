c2ffcd14b988e3f401ef831d21c21d66c4e740e9
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-28 10:51:34 +0100

    Add script for building driver for x64 and ARM64 and CAB file packaging
    Based on doc on https://learn.microsoft.com/nb-no/windows-hardware/drivers/dashboard/code-signing-attestation

---
+++ HidBattExt/BuildDriverRelease.bat
@@ -0,0 +1,13 @@
+:: Script must be run from a "Developer Command Prompt"
+
+echo Building driver:
+msbuild.exe  /nologo /verbosity:minimal /property:Configuration="Release";Platform="x64"   ..\HidBattExt.sln || exit /B 1
+msbuild.exe  /nologo /verbosity:minimal /property:Configuration="Release";Platform="ARM64" ..\HidBattExt.sln || exit /B 1
+
+echo Packaging driver binaries in CAB file:
+makecab.exe /f HidBattExt.ddf || exit /B 1
+
+echo Signing driver CAB:
+signtool.exe sign /a /fd sha256 disk1\HidBatttExt.cab || exit /B 1
+
+echo [done]
