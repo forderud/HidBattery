3726a661cd7784266561f15dc859011ac6becbfd
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-03-05 12:34:47 +0100

    Split x64 vs. arm64 binaries into separate CAB files.

--- HidBattExt/BuildDriverRelease.bat
+++ HidBattExt/BuildDriverRelease.bat
@@ -2,12 +2,14 @@
 
 echo Building driver:
 msbuild.exe  /nologo /verbosity:minimal /property:Configuration="Release";Platform="x64"   ..\HidBattExt.sln || exit /B 1
-::msbuild.exe  /nologo /verbosity:minimal /property:Configuration="Release";Platform="ARM64" ..\HidBattExt.sln || exit /B 1
+msbuild.exe  /nologo /verbosity:minimal /property:Configuration="Release";Platform="ARM64" ..\HidBattExt.sln || exit /B 1
 
 echo Packaging driver binaries in CAB file:
-makecab.exe /f HidBattExt.ddf || exit /B 1
+makecab.exe /f HidBattExt_x64.ddf || exit /B 1
+makecab.exe /f HidBattExt_arm64.ddf || exit /B 1
 
 echo Signing driver CAB:
-signtool.exe sign /a /fd sha256 disk1\HidBatttExt.cab || exit /B 1
+signtool.exe sign /a /fd sha256 disk1\HidBatttExt_x64.cab || exit /B 1
+signtool.exe sign /a /fd sha256 disk1\HidBatttExt_arm64.cab || exit /B 1
 
 echo [done]
