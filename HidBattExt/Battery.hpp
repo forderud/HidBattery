6810775826b1be6f4bebbb23e24186d18d84f746
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-03-14 13:51:55 +0100

    Unify context objects.

--- HidBattExt/Battery.hpp
+++ HidBattExt/Battery.hpp
@@ -21,20 +21,6 @@ struct BATT_STATE {
     WCHAR                           UniqueId[MAX_BATTERY_STRING_SIZE];
 };
 
-struct BATT_FDO_DATA {
-    // Battery class registration
-    void* ClassHandle;
-    WDFWAITLOCK                     ClassInitLock;
-    WMILIB_CONTEXT                  WmiLibContext;
-
-    // Battery state
-    WDFWAITLOCK                     StateLock;
-    ULONG                           BatteryTag;
-    BATT_STATE                   State;
-};
-
-WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(BATT_FDO_DATA, GetDeviceExtension);
-
 
 NTSTATUS InitializeBattery(_In_ WDFDEVICE Device);
 
