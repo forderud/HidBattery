dad98cbde30edb8911ffa902b763c194dec3f5b6
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-03-26 16:39:15 +0100

    Skip loading the driver on Windows 11 >=29550
    Fixes #86 by safeguarding against problems if the drier is loaded on recent Windows versions where it's no longer needed

--- HidBattExt/driver.cpp
+++ HidBattExt/driver.cpp
@@ -5,6 +5,29 @@
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT  DriverObject, _In_ PUNICODE_STRING RegistryPath ) {
     DebugPrint(DPFLTR_INFO_LEVEL, "HidBattExt: DriverEntry\n");
 
+    {
+        // Win11 build version that made HidBattExt redundant
+        OSVERSIONINFOEXW ver{};
+        ver.dwOSVersionInfoSize = sizeof(ver);
+        ver.dwMajorVersion = 10;
+        ver.dwMinorVersion = 0;
+        ver.dwBuildNumber = 29550;
+
+        ULONGLONG ConditionMask = 0;
+        VER_SET_CONDITION(ConditionMask, VER_MAJORVERSION, VER_GREATER_EQUAL);
+        VER_SET_CONDITION(ConditionMask, VER_MINORVERSION, VER_GREATER_EQUAL);
+        VER_SET_CONDITION(ConditionMask, VER_BUILDNUMBER, VER_GREATER_EQUAL);
+
+        NTSTATUS status = RtlVerifyVersionInfo(&ver, VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER, ConditionMask);
+
+        if (NT_SUCCESS(status)) {
+            // Windows 11 build 29550 or newer
+            DebugPrint(DPFLTR_ERROR_LEVEL, DML_ERR("HidBattExt: Driver no longer needed on Windows %d.%d.%d\n"), ver.dwMajorVersion, ver.dwMinorVersion, ver.dwBuildNumber);
+            DebugPrint(DPFLTR_ERROR_LEVEL, DML_ERR("HidBattExt: Please uninstall this driver.\n"));
+            return STATUS_NOT_SUPPORTED;
+        }
+    }
+
     WDF_DRIVER_CONFIG params = {};
     WDF_DRIVER_CONFIG_INIT(/*out*/&params, EvtDriverDeviceAdd);
     params.DriverPoolTag = POOL_TAG;
